e60d827158ff38d9125a193a714ef28d8f286091
marcusdaniel16 <matheussoares170110@gmail.com>
2026-06-01 15:54:04 +0000

    v2

--- calc_imc.py
+++ calc_imc.py
@@ -1,3 +1,8 @@
 #Matheus Soares: Recomendação Médica de Peso
 def calcular_imc(peso, altura):
     return peso, altura()
+#Marcus Daniel: parte 2
+if valor_imc < 25:
+    return "NORMAL"
+    else:
+        return "ACIMA DO PESO"
